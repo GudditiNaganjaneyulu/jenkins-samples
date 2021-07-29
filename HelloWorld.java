998a66bef87afcbc40edaff8a80a42f3897516f2
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2021-07-29 16:32:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello👋 world!😊welcome to jenkins sample program  you can want fallow me ❤ https://github.com/GudditiNaganjaneyulu ");
+    System.out.println("Hello👋 world!😊welcome to jenkins sample program  you can want fallow me ❤ https://github.com/GudditiNaganjaneyulu ❤");
   }
 }
